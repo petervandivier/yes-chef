b8a92c204bd79682a69a7340c112cffaf7fb0d32
petervandivier <petervandivier@gmail.com>
2019-05-09 16:30:59 +0100

    tokenize dir path, init *.conf resources

--- recipes/roll_your_own.rb
+++ recipes/roll_your_own.rb
@@ -1,10 +1,11 @@
 
-
 package 'postgresql-server' do
     action :install
 end
 
-directory '/var/lib/pgsql/data' do
+base_dir = '/var/lib/pgsql/data'
+
+directory "#{base_dir}" do
     owner 'postgres'
     group 'postgres'
     action :create
@@ -12,7 +13,23 @@ end
 
 execute "postgresql-setup initdb" do
     user "postgres"
-    not_if { ::File.exist?("/var/lib/pgsql/data/PG_VERSION")}
+    not_if { ::File.exist?("#{base_dir}/PG_VERSION")}
+end
+
+cookbook_file "#{base_dir}/postgresql.conf" do
+    source 'postgresql.conf'
+    owner 'postgres'
+    group 'postgres'
+    mode 0600
+    notifies :reload, 'service[postgresql]'
+end
+
+cookbook_file "#{base_dir}/pg_hba.conf" do
+    source 'pg_hba.conf'
+    owner 'postgres'
+    group 'postgres'
+    mode 0600
+    notifies :reload, 'service[postgresql]'
 end
 
 service 'postgresql' do
