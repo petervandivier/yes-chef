5b7d50fd4521ebcf75487fea06d50b7be9d061e8
petervandivier <petervandivier@gmail.com>
2019-07-26 13:25:18 +0100

    must specify non-default path for non-default templates

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -8,20 +8,21 @@ wal_arch  = node['pg']['hadr']['wal_archive']
         owner 'postgres'
         group 'postgres'
         mode 0o705
+        recursive true
     end
 end
 
-node.default['pg']['conf']['hba']['records'] << [{type: 'local', db: 'replication', user: 'postgres', method: 'peer', comment: 'required for basebackup'}]
+# node.default['pg']['conf']['hba']['records'] << {type: 'local', db: 'replication', user: 'postgres', method: 'peer', comment: 'required for basebackup'}
 
-node.default['pg']['conf']['file']['lines'] << [
-    {key: 'archive_command', value: "test ! -f #{wal_arch}/%f && cp %p #{wal_arch}/%f" }
-]
+# node.default['pg']['conf']['file']['lines'] << [
+#     {key: 'archive_command', value: "test ! -f #{wal_arch}/%f && cp %p #{wal_arch}/%f" }
+# ]
 
 template "#{node['pg']['etc']}/basebackup.sh" do
     owner 'postgres'
     group 'postgres'
     mode 0o701
-    source 'basebackup.sh.erb'
+    source 'backup/basebackup.sh.erb'
 end
 
 cron 'pg_basebackup' do
