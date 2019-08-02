4cc6226aff419c86522f7c11620a6239f40dad7c
petervandivier <petervandivier@gmail.com>
2019-08-02 13:40:06 +0100

    split hba deps out per-recipe, direct-append rows for backup hba

--- attributes/backup.rb
+++ attributes/backup.rb
@@ -1,8 +1,19 @@
 
+require 'csv'
+
 wal_arch = '/data/bkp/wal'
+resource_dir = File.dirname(File.expand_path(__FILE__))+ '/../resources/backup/'
+hba_csv = File.read(resource_dir + 'pg_hba.conf.csv')
 
 default['pg']['hadr']['base_bkp'] = '/data/bkp/base'
 default['pg']['hadr']['wal_archive'] = "#{wal_arch}"
 
 default['pg']['hadr']['name_fmt'] = 'date -u +%Y%m%d%H%M%S%z'
 default['pg']['hadr']['retention_days'] = 7
+
+hba_cols = CSV.parse_line(hba_csv)
+hba_hash = CSV.parse(hba_csv).map {|a| Hash[hba_cols.zip(a)]}
+hba_hash.shift # trim header tuple
+hba_hash.each do |record|
+    default['pg']['conf']['hba']['records'] << record
+end
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,26 +1,28 @@
 
-pg_conf_path = '/data/conf'
+require 'csv'
+
+pg_conf_dir = '/data/conf'
 pg_base = '/data/base'
 pg_log = '/data/log'
+resource_dir = File.dirname(File.expand_path(__FILE__))+ '/../resources/'
+hba_csv = File.read(resource_dir + 'pg_hba.conf.csv')
 
 default['pg']['base'] = "#{pg_base}"
 default['pg']['log']  = "#{pg_log}"
 default['pg']['etc']  = '/data/etc'
 default['pg']['bin']  = '/usr/pgsql-10/bin'
 
-default['pg']['conf']['path']          = "#{pg_conf_path}"
-default['pg']['conf']['file']['path']  = "#{pg_conf_path}/postgresql.conf"
+default['pg']['conf']['path']          = "#{pg_conf_dir}"
+default['pg']['conf']['file']['path']  = "#{pg_conf_dir}/postgresql.conf"
 default['pg']['conf']['file']['lines'] = [
 # FILE LOCATIONS
     {key: 'data_directory', value: "#{pg_base}/"},
-    {key: 'hba_file',       value: "#{pg_conf_path}/pg_hba.conf"},
-    {key: 'ident_file',     value: "#{pg_conf_path}/postgresql.conf"}
+    {key: 'hba_file',       value: "#{pg_conf_dir}/pg_hba.conf"},
+    {key: 'ident_file',     value: "#{pg_conf_dir}/postgresql.conf"}
 ]
-default['pg']['conf']['hba']['path']    = "#{pg_conf_path}/pg_hba.conf"
-default['pg']['conf']['hba']['records'] = []
 
-require 'csv'
-hba_csv = File.read(File.dirname(File.expand_path(__FILE__)) + '/../resources/pg_hba.conf.csv')
+default['pg']['conf']['hba']['path']    = "#{pg_conf_dir}/pg_hba.conf"
+default['pg']['conf']['hba']['records'] = []
 hba_cols = CSV.parse_line(hba_csv)
 hba_hash = CSV.parse(hba_csv).map {|a| Hash[hba_cols.zip(a)]}
 hba_hash.shift # trim header tuple
