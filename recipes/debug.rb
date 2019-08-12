d6cb4cb4a186c24a76f36c132b5e52c027929452
petervandivier <petervandivier@gmail.com>
2019-08-12 14:24:52 +0100

    basebackup.sh: rm cruft, push/pop dir, tar fmt. archive_cmd.sh: tar append wal seg. debug.rb: add pkgs, add heartbeat

--- recipes/debug.rb
+++ recipes/debug.rb
@@ -10,3 +10,34 @@ group 'postgres' do
     members %w(vagrant)
     action :modify
 end
+
+%w[
+    epel-release
+    jq
+    tree
+].each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+# user 'postgres' do
+#     manage_home true
+#     home "#{node['pg']['root']}"
+#     action :modify
+# end
+
+make_heartbeat_target=<<-EOF
+psql -U postgres -c "create table if not exists heartbeat ( ts timestamp with time zone );" 
+psql -U postgres -c "delete from heartbeat;" 
+psql -U postgres -c "insert into heartbeat(ts) select now();"
+EOF
+
+execute 'make heartbeat target' do
+    command make_heartbeat_target
+end
+
+cron 'heartbeat' do
+    minute '*'
+    command 'psql -U postgres -c "update heartbeat set ts = now() where not(pg_is_in_recovery());"'
+end
