b485821ed912fabb8434b69364cf57a3ad6c5f13
petervandivier <petervandivier@gmail.com>
2020-07-03 09:26:58 +0100

    ✨ add jq to default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 template '/etc/profile.d/bash_profile.sh' do 
     source 'bash_profile.sh'
 end
+
+# https://stackoverflow.com/a/45745410/4709762
+yum_package 'epel-release' do
+end
+yum_package 'jq' do
+end
